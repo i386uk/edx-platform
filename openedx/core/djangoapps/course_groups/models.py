c15f54d0c457c7af675fe8332f3234df18718675
sandroroux <aroux@edx.org>
2018-04-20 14:45:00 -0400

    fixup

--- openedx/core/djangoapps/course_groups/models.py
+++ openedx/core/djangoapps/course_groups/models.py
@@ -10,7 +10,6 @@ from django.core.exceptions import ValidationError
 from django.db import models, transaction
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
-
 from util.db import outer_atomic
 
 from opaque_keys.edx.django.models import CourseKeyField
